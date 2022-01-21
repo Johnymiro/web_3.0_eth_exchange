99bd642b03fac5d01b7f8b28bfb9158cc94e478e
Johny Miro <johny@Air-de-Johny.Home>
2022-01-21 17:20:46 +0000

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Starting Vite React app</div>;
+}
+
+export default App;
